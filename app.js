b6a6e24353f7a1932bd9c55a4611ee58896a2634
Jamesgit22 <jschroeder97@icloud.com>
2023-04-24 17:43:17 -0500

    Added post request to add notes once the save button is clicked. Still in dev

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // Import Express
 
 const express = require("express");
-const path = require('path')
+const path = require('path');
+const notesData = require('./db/db.json');
+const fs = require('fs');
 const port = 3000;
 const app = express();
 
@@ -12,6 +14,19 @@ app.use(express.static("public"));
 
 // Get request to load notes.html screen
 app.get('/notes', (req, res) => res.status(200).sendFile(path.join(__dirname, '/public/notes.html')))
+
+// Get previously saved notes from database
+app.get('/api/notes', (req, res) => res.json(notesData));
+
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body
+    console.log(JSON.stringify(req.body))
+    fs.writeFile('db/db.json', JSON.stringify(req.body), err => {
+        if (err) throw err
+
+        console.log("Added Note");
+    } )
+})
 
 
 // Add a listener for a port.
