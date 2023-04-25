d6f8322687d385e10472d74959ece56c13797b32
Jamesgit22 <jschroeder97@icloud.com>
2023-04-25 16:36:10 -0500

    Added post requst logic for posting a new note

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,9 +1,32 @@
-const notes = require('express').Router();
-const { readAndAppend } = require('../../helpers/fsUtils');
-const uuid = require('../../helpers/uuid');
-const notesData = require('../../db/db.json')
+const notes = require("express").Router();
+const { readAndAppend } = require("../../helpers/fsUtils");
+const uuid = require("../../helpers/uuid");
+const notesData = require("../../db/db.json");
 
-// notes.get('/', (req, res) => res.json('success'));
-notes.get('/', (req, res) => res.json(notesData));
+// Retrieve any existing notes from the database
+notes.get("/", (req, res) => res.json(notesData));
+
+// Add new notes to the existing database
+notes.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  // Check all variables are present
+  if (title && text) {
+    const newNote = {
+      title: title,
+      text: text,
+      id: uuid(),
+    };
+
+    readAndAppend(newNote, "././db/db.json");
+    res
+      .status(200)
+      .json(`Success: Added ${JSON.stringify(newNote)} to database.`);
+  } else {
+    res.status(400).json("Error in creating new note.");
+  }
+});
+
+// notes.delete
 
 module.exports = notes;
