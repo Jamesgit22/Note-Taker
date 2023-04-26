4fd5730698e344e489c34d247ec4ddef6c6a853d
Jamesgit22 <jschroeder97@icloud.com>
2023-04-26 18:12:19 -0500

    Verfied local endpoints

--- app.js
+++ app.js
@@ -3,53 +3,19 @@
 const express = require("express");
 // const path = require('path');
 // const notesData = require('./db/db.json');
-const fs = require('fs');
-const api = require('./routes/api/index.js')
-const html = require('./routes/html/index.js')
+const fs = require("fs");
+const api = require("./routes/api/index.js");
+const html = require("./routes/html/index.js");
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
-app.use('/api', api);
-app.use('/notes', html);
+app.use("/api", api);
+app.use("/notes", html);
 
-
-
-app.listen(port, () => console.log(`listening on port ${port} http://localhost:${port}`))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Get request to load notes.html screen
-
-// Get previously saved notes from database
-// app.get('/api/notes', (req, res) => res.json(notesData));
-
-// app.post('/api/notes', (req, res) => {
-//     const newNote = req.body
-//     console.log(JSON.stringify(req.body))
-//     fs.writeFile('db/db.json', JSON.stringify(req.body), err => {
-//         if (err) throw err
-
-//         console.log("Added Note");
-//     } )
-// })
-
-
-// Add a listener for a port.
+app.listen(port, () =>
+  console.log(
+    `listening on port ${port} https://hidden-badlands-10348.herokuapp.com , http://localhost:${port}`
+  )
+);
