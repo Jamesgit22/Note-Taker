8331b372a9f10d2eadb7f6e5dcff5e37537db0b5
Jamesgit22 <jschroeder97@icloud.com>
2023-04-27 14:23:20 -0500

    Changed delete endpoint

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,12 +1,12 @@
 const notes = require("express").Router();
-const { readAndAppend, readFromFile } = require("../../helpers/fsUtils");
+const { readAndAppend, readFromFile, writeToFile } = require("../../helpers/fsUtils");
 const uuid = require("../../helpers/uuid");
 const fs = require("fs");
 
 // Retrieve any existing notes from the database
 notes.get("/", (req, res) => {
   readFromFile("././db/db.json", "utf8").then(data => {
-    console.log(data);
+    // console.log(data);
     res.status(200).send(data);
   })
 
@@ -35,16 +35,18 @@ notes.post("/", (req, res) => {
 notes.delete("/:id", (req, res) => {
   readFromFile("././db/db.json", "utf8").then(data => {
     console.log(data);
-    const index = data.findIndex((note) => note.id === req.params.id);
+    console.log(req.params.id)
+    const parsedData = JSON.parse(data);
+    const index = parsedData.findIndex((note) => note.id === req.params.id);
 
     if (index === -1) {
       return res.status(404).send("Note not found");
     }
   
-    data.splice(index, 1);
-    writeToFile("../db/db.json", JSON.stringify(data));
+    parsedData.splice(index, 1);
+    writeToFile("././db/db.json", parsedData);
   
-    res.send("Note deleted successfully");
+    res.status(200).json({ok: true});
   });
 });
 
